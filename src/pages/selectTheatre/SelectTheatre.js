c315f926077d8ef32e6fe6bd4daaef8df03e313a
Virshree <virshreedesai19@gmail.com>
2022-08-08 17:30:14 +0530

    changes done in select theatre page

--- src/pages/selectTheatre/SelectTheatre.js
+++ src/pages/selectTheatre/SelectTheatre.js
@@ -1,29 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getMovieDetail } from "../../api/movie";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import "./selectTheatre.css";
 function SelectTheatre() {
+  const [selectTheatre, setSelectTheate] = useState({});
 
-  const [selectTheatre,setSelectTheate]=useState({});
+  const param = useParams();
+  const { movieName, movieId } = param;
+
+  useEffect(() => {
+    fetchMovies(movieId);
+  }, []);
+  const fetchMovies = () => {
+    getMovieDetail(movieId)
+      .then((res) => {
+        const { status, data } = res;
+        if (status === 200) {
+          console.log(data);
+          setSelectTheate(data);
+        }
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const {
+    description = " ",
+    language = " ",
+    releaseStatus = " ",
+    director = "",
+    releaseDate = "",
+  } = selectTheatre;
+
   return (
     <div>
       <Header hideSearch={true} />
 
       <div className="select-main">
+        <div className="select-theatre">
+          <h2 className="p-2">{movieName}</h2>
+          <hr />
 
-        <div>
-          <h3></h3>
+          <span className="text-center m-2  movie-description">
+            {description}
+          </span>
+          <span className="text-center m-2  movie-language">{language}</span>
+          <span className="text-center m-2 movie-status">{releaseStatus}</span>
+          <br />
+          <br />
+          <div>
+            <h4>
+              <i>Director By :{director}</i>
+            </h4>
+
+            <span>Date:{releaseDate}</span>
+          </div>
         </div>
-
-
-
-
-
-
-
-
-
-
       </div>
       <Footer />
     </div>
